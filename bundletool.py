d55d13671c16c1327ba82f1683527a68dc17d0f2
WS <>
2024-06-24 19:17:53 +0800

    update android.jar to 34

--- bundletool.py
+++ bundletool.py
@@ -54,7 +54,7 @@ def get_base_dir() -> str:
 
 APKTOOL_PATH = os.path.join(get_base_dir(), "tools", "apktool_2.9.3.jar")
 AAPT2_PATH = os.path.join(get_base_dir(), "tools", "aapt2", get_system(), "aapt2")
-ANDROID_JAR_PATH = os.path.join(get_base_dir(), "tools", "android_30.jar")
+ANDROID_JAR_PATH = os.path.join(get_base_dir(), "tools", "android_34.jar")
 BUNDLETOOL_TOOL_PATH = os.path.join(get_base_dir(), "tools", "bundletool-all-1.6.1.jar")
 
 KEYSTORE = os.path.join(get_base_dir(), "tools", "luojian37.jks")
